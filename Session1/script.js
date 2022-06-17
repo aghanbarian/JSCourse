770565d3cb4aeba6828445526b0a71a5ef341343
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-06-17 15:29:23 +0430

    session1

---
+++ Session1/script.js
@@ -0,0 +1,16 @@
+//alert('hi in Alert')
+document.write('hi from document ')
+document.getElementById('myId').innerHTML = 'Hi from JS'
+
+let studentCount = 70
+let staffCount = 10
+var first_name = 'hassan'
+const last_name = 'ahmadi'
+
+console.log('TotalCount : ')
+console.log(studentCount + staffCount)
+
+console.log(first_name)
+first_name = 'ali'
+console.log(first_name)
+console.log(last_name)
