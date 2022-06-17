47f1bcbf72a1be0f3b9567459346d86edc4d26be
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-06-17 17:08:30 +0430

    Session 1 hour 2

--- Session1/script.js
+++ Session1/script.js
@@ -7,14 +7,60 @@ let staffCount = 10
 var first_name = 'hassan'
 const last_name = 'ahmadi'
 
-console.log('TotalCount : ')
-console.log(studentCount + staffCount)
+// console.log('TotalCount : ')
+// console.log(studentCount + staffCount)
 
-console.log(first_name)
-first_name = 'ali'
-console.log(first_name)
-console.log(last_name)
-console.log('github initiated ')
-console.log('github sync')
-document.write('<br/>')
-document.write('new changes in log')
+// console.log(first_name)
+// first_name = 'ali'
+// console.log(first_name)
+// console.log(last_name)
+// console.log('github initiated ')
+// console.log('github sync')
+// document.write('<br/>')
+// document.write('new changes in log')
+
+/* Case sensitive */
+let age = 30
+let AGE = 50
+console.log(age)
+console.log(AGE)
+
+/*one line var definition */
+let name,
+  lastname = ''
+
+name = 'david'
+lastname = 'robertson'
+
+console.log(name)
+console.log(lastname)
+
+/*Numbers types */
+a = 10
+b = 18.5
+c = -16.5
+d = 3e4
+f = 4.2e7
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(f)
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof f)
+
+if (typeof c == 'number') {
+  console.log(true)
+}
+
+console.log(typeof name)
+
+/* null value */
+
+let n = null
+console.log(n)
+console.log(typeof n)
