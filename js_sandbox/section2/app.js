542d8abc2011a178133ed9604844c35c69b28039
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-06-30 16:35:20 +0430

    section 2 part 3 end

---
+++ js_sandbox/section2/app.js
@@ -0,0 +1,54 @@
+// console log
+
+console.log('Hello World !')
+console.log(12635)
+console.log(false)
+// var greeting = 'Hello !'
+// console.log(greeting)
+console.log([1, 4, 5, 6, 8])
+console.log({ a: 2, b: 56 })
+console.table({ a: 2, b: 56 })
+console.error('this is some error')
+/* 
+multi 
+line 
+command 
+*/
+// console.clear()
+// console.warn('some warning to you ')
+// console.time('Hello')
+// console.log(greeting)
+// console.log(greeting)
+// console.log(greeting)
+// console.log(greeting)
+// console.log(greeting)
+// console.log(greeting)
+// console.timeEnd('Hello')
+
+// var let const
+
+var name = 'john Doe'
+console.log(name)
+name = 'doe smith'
+console.log(name)
+
+var greeting
+console.log(greeting)
+greeting = 'Hello World !'
+console.log(greeting)
+
+const a = 100
+
+const person = {
+  name: 'ahmad',
+  age: 30,
+}
+
+person.name = 'sara'
+person.age = 32
+console.log(person)
+
+const numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers[3] = 10
+numbers.push(8)
+console.log(numbers)
