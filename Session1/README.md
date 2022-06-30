9387248b9a272a408a63f8f9878f0cc53c011b2f
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-06-30 15:03:24 +0430

    End Of Session 3 and Start of Brad

---
+++ Session1/README.md
@@ -0,0 +1,3 @@
+# Session1
+session 1 ends with var type conversion 
+the session 2 ends with decision making commands
--- Session1/scriptsession3.js
+++ Session1/scriptsession3.js
@@ -81,61 +81,130 @@ let rand = Math.floor(Math.random() * (maxN - minN + 1) + minN)
 //   counter--
 // }
 
-console.clear()
-counter = 50
-do {
+// console.clear()
+// counter = 50
+// do {
+//   console.log(counter)
+//   counter -= 10
+// } while (counter > 0)
+
+// let array = [1, 3, 7, 8, 'ali', 2, 3, 3, 'b', 4, 8]
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index]
+//   console.log(element)
+// }
+
+// for (let i = 9; i > 0; i--) {
+//   console.log(i)
+// }
+
+// // let colors = new Array('red', 'blue', 'green')
+// let colors = ['red', 'blue', 'green']
+// console.log(colors)
+
+// let info = ['david', 'johnson', 30, true, 5000]
+// console.log('====================================')
+// console.log(info)
+// info[0] = 'john'
+// console.log(info)
+// console.log('====================================')
+// info[10] = '2020'
+// console.log(info)
+// info[2] = 'lovely'
+// console.log(info)
+// info[5] = ['1', '2', '4']
+// console.log(info)
+
+// let cars = ['benz', 'pride', 'vanet']
+// const cars3 = ['benz', 'pride', 'vanet']
+
+// cars[0] = '1'
+// console.log(cars)
+// console.log(cars.length)
+
+// cars2 = cars
+// console.log(cars == cars2)
+// console.log(cars3 == cars)
+// cars[0] = 'han'
+// console.log(cars)
+// console.log(cars2)
+
+// let [a, b, c] = [4, 5, 6]
+// console.log(a, b, c)
+
+// // cars.push('paykan')
+// // console.log(cars)
+// // cars.pop('paykan')
+// // console.log(cars)
+// console.clear()
+// console.log(Array.isArray(cars))
+// cars.push('BMW')
+// cars.push('tucsun')
+// cars.sort((a, b) => a > b)
+// let result = cars.find((element) => element > 'han')
+// console.log(result)
+// console.log(cars instanceof Array)
+// console.log(cars instanceof Object)
+// //to string with default seperator and specific seperator
+// // console.log(String(cars))
+// // console.log(cars.toString())
+// // console.log(cars.join("'"))
+
+// // //slice method
+// // console.log(cars.slice(0, 2))
+// // let slicedArr = cars.slice(2, 4)
+// // console.log(slicedArr)
+
+// //remove and add elements with splice
+// // console.log(cars)
+// // cars.splice(1, 0, 'ali', 'hassan', 'majid', 'saeed')
+// // console.log(cars)
+
+// // cars.reverse()
+// // console.log(cars)
+// // cars.sort()
+// // console.log(cars)
+// // console.log(cars.indexOf('hassan'))
+// // console.log(cars.includes('majid'))
+
+// console.log(cars.concat(cars3))
+// newCar = [cars, cars3]
+// console.log(newCar)
+
+// speardCars = [...cars, ...cars3]
+// console.log(speardCars)
+
+// arr1 = [(name) => 'ahmad', (famil) => 'ghanbarian']
+// arr2 = [(age) => 30, (car) => 'BMW']
+
+// console.log(arr1 + ' -- ' + arr2)
+// arr3 = [...arr1, ...arr2]
+// console.log(String(arr3))
+
+// let boys = ['ali', 'hassan', 'javad', 'saeed']
+
+// for (let c = 0; c < boys.length; c++) {
+//   console.log(boys[c])
+// }
+
+let numArr = [2, 5, 8, 3, 4, 10, 12]
+
+// for (let c = 0; c < numArr.length; c++) {
+//   numArr[c] *= 2
+//   console.log(numArr[c])
+// }
+
+// console.log(numArr)
+
+// for (let num of numArr) {
+//   num *= 2
+//   console.log(num)
+// }
+console.clear
+console.log(numArr)
+let counter = 0
+while (counter < numArr.length) {
+  console.log(numArr[counter])
+  counter++
   console.log(counter)
-  counter -= 10
-} while (counter > 0)
-
-let array = [1, 3, 7, 8, 'ali', 2, 3, 3, 'b', 4, 8]
-for (let index = 0; index < array.length; index++) {
-  const element = array[index]
-  console.log(element)
 }
-
-for (let i = 9; i > 0; i--) {
-  console.log(i)
-}
-
-// let colors = new Array('red', 'blue', 'green')
-let colors = ['red', 'blue', 'green']
-console.log(colors)
-
-let info = ['david', 'johnson', 30, true, 5000]
-console.log('====================================')
-console.log(info)
-info[0] = 'john'
-console.log(info)
-console.log('====================================')
-info[10] = '2020'
-console.log(info)
-info[2] = 'lovely'
-console.log(info)
-info[5] = ['1', '2', '4']
-console.log(info)
-
-const cars = ['benz', 'pride', 'vanet']
-const cars3 = ['benz', 'pride', 'vanet']
-
-cars[0] = '1'
-console.log(cars)
-console.log(cars.length)
-
-cars2 = cars
-console.log(cars == cars2)
-console.log(cars3 == cars)
-cars[0] = 'han'
-console.log(cars)
-console.log(cars2)
-
-let [a, b, c] = [4, 5, 6]
-console.log(a, b, c)
-
-cars.push('paykan')
-console.log(cars)
-cars.pop('paykan')
-console.log(cars)
-
-console.log(Array.isArray(cars))
-console.log(String(cars.join('"')))
