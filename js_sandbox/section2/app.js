3df413a3f720915ebb9cc2b4fbb41e4a630c7a2d
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-05 17:36:26 +0430

    Switch and Case

--- js_sandbox/section2/app.js
+++ js_sandbox/section2/app.js
@@ -1,55 +1,42 @@
-let id = 100
+let color = 'blue'
 
-if (id == 100) {
-  console.log('Correct')
-} else {
-  console.log('Incorrect')
+switch (color) {
+  case 'red':
+    console.log(`the color is ${color} and is my favorit`)
+    break
+  case 'blue':
+    console.log(`the color is ${color} and is the best`)
+    break
+  default:
+    console.log(`Colorful World `)
 }
 
-id = '100'
-
-if (id === 100) {
-  console.log('Correct')
-} else {
-  console.log('Incorrect')
+let day
+today = new Date()
+switch (today.getDay()) {
+  case 0:
+    day = 'monday'
+    break
+  case 1:
+    day = 'tuesday'
+    break
+  case 2:
+    day = 'wendsday'
+    break
+  case 3:
+    day = 'tursday'
+    break
+  case 4:
+    day = 'friday'
+    break
+  case 5:
+    day = 'saturday'
+    break
+  case 6:
+    day = 'sunday'
+    break
+  default:
+    day = 'not available'
 }
 
-id = 101
-
-if (id != 100) {
-  console.log('not Correct')
-} else {
-  console.log('Correct')
-}
-
-let nd = 102
-if (nd) {
-  console.log(`the value of variable is ${nd}`)
-} else {
-  console.log('the variable is not ddefined')
-}
-
-if (id > 100) {
-  console.log('the id is correct')
-} else {
-  console.log('Id is not correct')
-}
-
-name = 'steve'
-age = 16
-
-if (age > 0 && age <= 12) {
-  console.log(`${name} is a child`)
-} else if (age >= 13 && age < 19) {
-  console.log(`${name} is a teenager`)
-} else {
-  console.log(`${name} is adult`)
-}
-
-if (age < 16 || age > 65) {
-  console.log(`${name} can not run in race`)
-} else {
-  console.log(`${name} is registered for race`)
-}
-
-console.log(age >= 13 ? 'Teenager and elder' : `${name} is Child`)
+console.log(day)
