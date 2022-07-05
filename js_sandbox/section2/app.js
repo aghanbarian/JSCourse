08629b1922e3b592c3a8dba7e7168d0454ab41e7
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-05 09:03:38 +0430

    Literal Sentences

--- js_sandbox/section2/app.js
+++ js_sandbox/section2/app.js
@@ -1,21 +1,30 @@
-// some arrays
+let name = 'john doe'
+let job = 'Web Developer'
+let city = 'Tehran'
+let age = 35
 
-let Numbers = [45, 22, 34, 76, 11, 12]
-let Numbers2 = new Array(19, 2, 12, 45, 23, 11, 4)
-let fruits = ['Apple', 'Banana', 'Orange', 'Pear']
-let Mixed = ['ahmad', 30, 'Ghanbarian', 'Tehran Majidieh']
-let val
+literalStr =
+  '<ul><li>name :' +
+  name +
+  '</li>' +
+  '<li>Job : ' +
+  job +
+  '</li>' +
+  '<li>City : ' +
+  city +
+  '</li>' +
+  '<li>age : ' +
+  age +
+  '</li>' +
+  '</ul>'
 
-console.log(Numbers)
-val = Numbers.length
-val = Array.isArray(Numbers2)
-val = Numbers[3]
-Numbers[2] = 100
-val = Numbers.indexOf(12)
-Numbers.push(44)
+// document.body.innerHTML = literalStr
 
-Numbers.unshift(999)
-Numbers.pop()
+literalStrTag = `<ul>
+              <li>Name : ${name}</li>
+              <li>Job : ${job}</li>
+              <li>Age : ${age}</li>
+              <li>city : ${city}</li>
+              </ul>`
 
-console.log(Numbers)
-console.log(val)
+document.body.innerHTML = literalStr + '<br />' + literalStrTag
