22f91955f068eb4972ed7bfae94187939ed68a1e
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-06 22:31:13 +0430

    Scope of variable Const and let

--- js_sandbox/section2/app.js
+++ js_sandbox/section2/app.js
@@ -1,47 +1,23 @@
-// window object
+//global variables
+var a = 1
+let b = 2
+const c = 3
 
-// window.console.log('hi')
+function test() {
+  var a = 3
+  let b = 6
+  const c = 10
 
-// let input = prompt()
-// console.log(input)
-// alert(input)
+  console.log('local of function : ', a, b, c)
+}
 
-// if (confirm('Are You sure?')) {
-//   console.log('YES')
-// } else {
-//   console.log('No')
-// }
+test()
 
-let val
+if (true) {
+  var a = 7
+  let b = 8
+  const c = 12
+  console.log('if scope : ', a, b, c)
+}
 
-val = window.outerHeight
-val = window.outerWidth
-
-let val1 = window.innerHeight
-let val2 = window.innerWidth
-let val3
-
-val1 = window.scrollY
-val2 = window.scrollX
-
-val1 = window.location
-val2 = window.location.hostname
-val3 = window.location.port
-val1 = window.location.href
-val2 = window.location.search
-
-//window.location.href = 'http://google.com'
-// window.location.reload()
-
-// console.log(val1, ' ', val2, ' ', val3)
-
-// window.history.go(-1)
-
-val = window.navigator
-val = window.navigator.appName
-val = window.navigator.appVersion
-val = window.navigator.userAgent
-val = window.navigator.platform
-val = window.navigator.vendor
-val = window.navigator.language
-console.log(val)
+console.log('Global variables : ', a, b, c)
