379e497add10e8cadca3e7835c83d54e1d7a706d
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-08 14:16:06 +0430

    one element selector

--- js_sandbox/section2/app.js
+++ js_sandbox/section2/app.js
@@ -1,30 +1,54 @@
-let val
+// documnet. get element by id
 
-val = document
-val = document.all
-val = document.all[3]
+console.log(document.getElementById('task-title'))
 
-// allArray = Array.from(document.scripts)
-// allArray.forEach(function (element) {
-//   console.log(element)
-// })
+console.log(document.getElementById('task-title').id)
+console.log(document.getElementById('task-title').className)
 
-val = document.head
-val = document.body
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
+let anElement
 
-val = document.links
-val = document.forms
+anElement = document.getElementById('task-title')
 
-val = document.images
+anElement.style.backgroundColor = '#666'
+anElement.style.color = '#FFF'
+anElement.style.padding = '2px'
+anElement.textContent = 'Task List'
+anElement.innerText = 'My Task'
+//anElement.innerHTML = '<span style="color:yellow">Task List Span</span>'
 
-valArr = Array.from(document.scripts)
+anElement = document.querySelector('#task-title')
 
-valArr.forEach(function (element) {
-  console.log(element)
-})
+anElement.style.color = 'RED'
 
-console.log(val)
+anElement = document.querySelector('.collection-item')
+
+anElement.style.color = 'ORANGE'
+
+anElement = document.querySelector('a')
+
+anElement.style.color = 'GREEN'
+
+anElement = document.querySelector('a')
+
+anElement.style.color = 'GREEN'
+anElement = document.querySelector('li')
+
+anElement.style.color = 'RED'
+
+anElement = document.querySelector('ul li')
+anElement.style.color = 'brown'
+
+anElement = document.querySelector('li:last-child')
+anElement.style.color = 'brown'
+
+anElement = document.querySelector('li:nth-child(3)')
+anElement.style.color = 'BLUE'
+
+anElement = document.querySelector('li:nth-child(4)')
+anElement.textContent = 'Anothert '
+
+anElement = document.querySelector('li:nth-child(odd)')
+anElement.style.background = 'Orange'
+
+anElement = document.querySelector('li:nth-child(even)')
+anElement.style.background = '#ccc'
