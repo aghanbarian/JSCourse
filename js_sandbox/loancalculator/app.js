5a1de68816b5c3841d5a35a936c285bd174f1ba8
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-13 23:41:33 +0430

    Calculator Part 2

---
+++ js_sandbox/loancalculator/app.js
@@ -0,0 +1,57 @@
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateLoanDetail)
+
+function calculateLoanDetail(e) {
+  console.log('====================================')
+  console.log('Calculating...')
+  console.log('====================================')
+
+  const amount = document.getElementById('amount')
+  const interest = document.getElementById('interest')
+  const years = document.getElementById('years')
+  const monthlyPayment = document.getElementById('monthly-payment')
+  const totalPayment = document.getElementById('total-payment')
+  const totalInterest = document.getElementById('total-interest')
+
+  let principal = parseFloat(amount.value)
+  let calculatedInterest = parseFloat(interest.value) / 100 / 12
+  let calculatedPayments = parseFloat(years.value) * 12
+
+  //compute Monthly payemnt
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+  const monthly = (principal * x * calculatedInterest) / (x - 1)
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2)
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2)
+  } else {
+    console.log('please check your numbers')
+    showError('Please Check Your Parameters')
+  }
+
+  //show error
+  function showError(err) {
+    //create div
+    const errorDiv = document.createElement('div')
+
+    const card = document.querySelector('.card')
+    const heading = document.querySelector('.heading')
+    errorDiv.className = 'alert alert-danger'
+    errorDiv.appendChild(document.createTextNode(err))
+
+    card.insertBefore(errorDiv, heading)
+
+    //clear error message
+
+    setTimeout(clearError, 3000)
+  }
+
+  function clearError() {
+    document.querySelector('.alert').remove()
+  }
+
+  e.preventDefault()
+}
