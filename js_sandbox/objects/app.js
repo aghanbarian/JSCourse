b1e69d86e31b4f7e2c882aa531d467db84b86c2a
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-15 15:37:02 +0430

    Object 2 Built in Constructor

--- js_sandbox/objects/app.js
+++ js_sandbox/objects/app.js
@@ -1,25 +1,63 @@
-const brad = {
-  name: 'brad',
-  age: 30,
+let name1 = 'jeff'
+let name2 = new String('jeff')
+
+console.log(name1)
+console.log(name2)
+
+console.log(typeof name1)
+console.log(typeof name2)
+
+if (name1 === 'jeff') {
+  console.log('YES')
+} else {
+  console.log('NO')
 }
 
-console.log(brad)
-console.log(brad.age)
-
-//construcor function
-
-function Person(name, dob) {
-  this.name = name
-  this.birthday = new Date(dob)
-  this.calculateAge = function () {
-    const diff = Date.now() - this.birthday.getTime()
-    const ageDate = new Date(diff)
-    return Math.abs(ageDate.getUTCFullYear() - 1970)
-  }
+if (name2 === 'jeff') {
+  console.log('YES')
+} else {
+  console.log('NO')
 }
 
-let bradPerson = new Person('brad', '9-10-1966')
-let johnPerson = new Person('John', '10-02-1988')
+if (name2 == 'jeff') {
+  console.log('YES')
+} else {
+  console.log('NO')
+}
 
-console.log(bradPerson, bradPerson.calculateAge())
-console.log(johnPerson, johnPerson.calculateAge())
+//Numbers
+
+let num1 = 10
+let num2 = new Number(5)
+
+console.log(num1)
+console.log(num2)
+
+console.log(typeof num1)
+console.log(typeof num2)
+
+//function
+
+const getsum1 = function (x, y) {
+  return x + y
+}
+
+const getsum2 = new Function('x', 'y', 'return x+y')
+
+console.log(getsum1(10, 20))
+console.log(getsum2(2, 7))
+
+//objects
+const john = { name: 'john' }
+
+console.log(john)
+
+const john2 = new Object({ name: 'john' })
+
+console.log(john2)
+
+const arr1 = [1, 2, 3, 4]
+const arr2 = new Array([1, 2, 3, 4])
+
+console.log(typeof arr1)
+console.log(typeof arr2)
