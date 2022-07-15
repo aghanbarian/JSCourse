e1318f9755a095826d59f4db3a331efbc1d891a7
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-15 20:07:52 +0430

    Object 7 Class Extends

--- js_sandbox/objects/app.js
+++ js_sandbox/objects/app.js
@@ -35,3 +35,34 @@ console.log(mary.calculateAge())
 console.log('====================================')
 
 console.log(Person.addNumbers(1, 2))
+
+class Customer extends Person {
+  constructor(name, family, bod, phone, membership) {
+    super(name, family, bod)
+
+    this.phone = phone
+    this.membership = membership
+  }
+
+  greeting() {
+    return `Hello There ${this.firstName} ${this.LastName} welcome to our company`
+  }
+
+  static getMembershipCost() {
+    return 500
+  }
+}
+
+const aCustomer = new Customer(
+  'ahmad',
+  'jefferson',
+  '10-10-1990',
+  '565-789-8989',
+  'standard'
+)
+
+console.log(aCustomer)
+
+console.log(aCustomer.greeting())
+
+console.log(Customer.getMembershipCost())
