e59ff8d3704fb609f49303ffbaecc6bf3cb24418
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-15 19:07:27 +0430

    Object 4 Inheritance prototype

--- js_sandbox/objects/app.js
+++ js_sandbox/objects/app.js
@@ -2,11 +2,6 @@ function Person(firstname, lastname, bod) {
   this.firstName = firstname
   this.lastName = lastname
   this.birthDay = new Date(bod)
-  // this.calculateAge = function () {
-  //   const diff = Date.now() - this.birthDay.getTime()
-  //   const ageDate = new Date(diff)
-  //   return Math.abs(ageDate.getUTCFullYear() - 1970)
-  // }
 }
 
 Person.prototype.calculateAge = function () {
@@ -34,3 +29,26 @@ console.log(mary.getBirthDate())
 console.log(mary.hasOwnProperty('lastName'))
 console.log(mary.hasOwnProperty('getFullName'))
 console.log(mary.hasOwnProperty('calculateAge'))
+
+function Customer(firstname, lastname, bod, phone, membership) {
+  Person.call(this, firstname, lastname, bod)
+
+  this.phone = phone
+  this.membership = membership
+}
+
+Customer.prototype = Object.create(Person.prototype)
+
+const customer1 = new Customer(
+  'ali',
+  'ghanbarian',
+  '2-10-1981',
+  '+989123055980',
+  'standard'
+)
+
+Customer.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName} welcome to our company`
+}
+console.log(customer1)
+console.log(customer1.getFullName())
