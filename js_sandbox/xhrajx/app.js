e5261443c5bfc5864262ee94959d391350543025
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-22 16:55:26 +0430

    Ajax part4 JSON input

--- js_sandbox/xhrajx/app.js
+++ js_sandbox/xhrajx/app.js
@@ -1,41 +1,49 @@
-document.getElementById('button').addEventListener('click', loadData)
+document.getElementById('button1').addEventListener('click', loadCustomer)
+document.getElementById('button2').addEventListener('click', loadCustomers)
 
-function loadData() {
-  //create XHR
-
+function loadCustomer(e) {
   const xhr = new XMLHttpRequest()
-
-  xhr.open('GET', 'data.txt', true)
-
-  // xhr.onload = function () {
-  //   if (this.status === 200) {
-  //     console.log(xhr.responseText)
-  //     document.getElementById('output').innerHTML = xhr.responseText
-  //   }
-  // }
-
-  //HTTP Request statuses
-  // 200: ok
-  // 403:forbiddenn
-  // 404:not found
-
-  xhr.onreadystatechange = function () {
-    if (xhr.status === 200 && xhr.readyState === 4) {
-      let resStr = ''
-      for (let i = 0; i < 10; i++) {
-        resStr += `<h1>
-        ${xhr.responseText}</h1>,<br />`
-      }
-      document.getElementById('output').innerHTML = resStr
+  xhr.open('GET', 'customer.json', true)
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(xhr.responseText)
+      const customer = JSON.parse(this.responseText)
+      let output = `
+      <ul>
+      <li>ID : ${customer.id}</li>
+      <li>Name : ${customer.name}</li>
+      <li>Lats Name : ${customer.lastName}</li>
+      <li>Name : ${customer.age}</li>
+      </ul>
+      `
+      console.log(output)
+      document.getElementById('customer').innerHTML = output
     }
   }
 
-  // readyState values
-  // 0 : request not initialized
-  // 1 : server connection stablished
-  // 2 : request recieved
-  // 3 : processing request
-  // 4 : request finished and response is ready
+  xhr.send()
+}
 
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.open('GET', 'customers.json', true)
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const customers = JSON.parse(this.responseText)
+      let output = ''
+      customers.forEach(function (customer) {
+        output += `
+        <ul>
+        <li>ID : ${customer.id}</li>
+        <li>Name : ${customer.name}</li>
+        <li>Lats Name : ${customer.lastName}</li>
+        <li>Name : ${customer.age}</li>
+        </ul> `
+      })
+      document.getElementById('customers').innerHTML = output
+    }
+  }
   xhr.send()
 }
