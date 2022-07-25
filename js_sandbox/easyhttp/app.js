539b2a1282fc4c2465f4c6d584b104d8fbab6bd5
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-07-25 22:44:26 +0430

    easyhttp Complete,post,put,delete

--- js_sandbox/easyhttp/app.js
+++ js_sandbox/easyhttp/app.js
@@ -2,21 +2,108 @@ const http = new easyHTTP()
 
 //Get Posts
 
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
-  if (err) {
-    console.log(err)
-  } else {
-    let pposts = JSON.parse(posts)
-    let output = ''
-    pposts.forEach(function (post) {
-      output += `<li>Title : ${post.title} <br /> Desc : ${post.body}</li>`
-    })
-    document.getElementById('output').innerHTML = `<ul>${output}</ul>`
-  }
-})
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     let pposts = JSON.parse(posts)
+//     let output = ''
+//     pposts.forEach(function (post) {
+//       output += `<li>ID: ${post.id} <br /> Title : ${post.title} <br /> Desc : ${post.body}</li>`
+//     })
+//     document.getElementById('output').innerHTML = `<ul>${output}</ul>`
+//   }
+// })
+//get single post
 
-// posts.forEach(function (post) {
-//   output += `<li>Title : ${post.title} <br /> Desc : ${post.body}</li>`
+function getPost(numberPost) {
+  if (numberPost !== 1) {
+    http.get(
+      'https://jsonplaceholder.typicode.com/posts',
+      function (err, posts) {
+        if (err) {
+          console.log(err)
+        } else {
+          let pposts = JSON.parse(posts)
+          let output = ''
+          pposts.forEach(function (post) {
+            output += `<li>ID: ${post.id} <br /> Title : ${post.title} <br /> Desc : ${post.body}</li>`
+          })
+          document.getElementById('output').innerHTML = `<ul>${output}</ul>`
+        }
+      }
+    )
+  } else {
+    http.get(
+      'https://jsonplaceholder.typicode.com/posts/1',
+      function (err, post) {
+        let output = ''
+        if (err) {
+          console.log(err)
+        } else {
+          let aPost
+          aPost = JSON.parse(post)
+          console.log(aPost)
+
+          output += `<h1>ID: ${aPost.id} <br /> Title : ${aPost.title} <br /> Desc : ${aPost.body}</h1>`
+        }
+        document.getElementById('output').innerHTML = `${output}`
+      }
+    )
+  }
+}
+
+// getPost(2)
+
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts)
+//   }
 // })
 
-// document.body.innerHTML = output
+data = { title: 'post by ahmad', body: 'a post frm martian' }
+
+function doPost() {
+  http.post(
+    'https://jsonplaceholder.typicode.com/posts',
+    data,
+    function (err, post) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(post)
+      }
+    }
+  )
+}
+
+function doPut(n, data) {
+  http.put(
+    `https://jsonplaceholder.typicode.com/posts/${n}`,
+    data,
+    function (err, post) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(post)
+      }
+    }
+  )
+}
+
+function doDelete() {
+  http.delete(
+    `https://jsonplaceholder.typicode.com/posts/1`,
+    function (err, response) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(response)
+      }
+    }
+  )
+}
+
+
--- js_sandbox/easyhttp/easyhttp.js
+++ js_sandbox/easyhttp/easyhttp.js
@@ -19,3 +19,42 @@ easyHTTP.prototype.get = function (url, callback) {
 }
 
 //make a hhtp post request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true)
+  this.http.setRequestHeader('Content-type', 'application/json')
+
+  let self = this
+  this.http.onload = function () {
+    callback(null, self.http.responseText)
+  }
+
+  this.http.send(JSON.stringify(data))
+}
+
+//make a http put request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true)
+  this.http.setRequestHeader('Content-type', 'application/json')
+
+  let self = this
+  this.http.onload = function () {
+    callback(null, self.http.responseText)
+  }
+
+  this.http.send(JSON.stringify(data))
+}
+
+//make a hhtp delte request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true)
+  let self = this
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, 'Post Deleted')
+    } else {
+      callback('Error : ' + self.http.status)
+    }
+  }
+
+  this.http.send()
+}
