3b4497993bcee51100cbe02a6478b4d6c4df4b06
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-08-18 18:49:58 +0430

    generator

--- js_sandbox/iterartorsequnce/app.js
+++ js_sandbox/iterartorsequnce/app.js
@@ -27,20 +27,58 @@ let namesArr = ['jack', 'jill', 'john']
 let names = nameIterator(namesArr)
 
 // console.log(names)
-console.log(names.next().value)
-console.log(names.next().value)
-console.log(names.next().value)
-console.log(names.prev().value)
-console.log(names.prev().value)
-console.log(names.prev().value)
+// console.log(names.next().value)
+// console.log(names.next().value)
+// console.log(names.next().value)
+// console.log(names.prev().value)
+// console.log(names.prev().value)
+// console.log(names.prev().value)
 
-names.update('ali')
-names.reset()
-console.log(names.next().value)
-console.log(names.next().value)
-console.log(names.next().value)
+// names.update('ali')
+// names.reset()
+// console.log(names.next().value)
+// console.log(names.next().value)
+// console.log(names.next().value)
 
-names.reset()
-console.log(names.next().value)
-console.log(names.next().value)
-console.log(names.next().value)
+// names.reset()
+// console.log(names.next().value)
+// console.log(names.next().value)
+// console.log(names.next().value)
+
+function* sayNames() {
+  yield 'ali'
+  yield 'ahmad'
+  yield 'hasan'
+  yield 'masood'
+  yield 'farhad'
+  yield 'amin'
+}
+
+const nameGen = sayNames()
+
+console.log(nameGen.next())
+console.log(nameGen.next())
+console.log(nameGen.next())
+
+console.log(nameGen.next())
+console.log(nameGen.next())
+console.log(nameGen.next())
+console.log(nameGen.next())
+
+function* genId() {
+  index = 0
+
+  while (true) {
+    yield index++
+  }
+}
+
+let gen = genId()
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
