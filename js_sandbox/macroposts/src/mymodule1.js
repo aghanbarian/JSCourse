b4d8caa96f29b6d91c4d0b5b8db89f6873d713e5
Ahmad Ghanbarian <aghanbarian@yahoo.com>
2022-09-02 19:04:13 +0430

    ES2015-2016-2017 diffs

---
+++ js_sandbox/macroposts/src/mymodule1.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'ahmad',
+  email: 'aghanbarian@yahoo.com',
+}
---
+++ js_sandbox/macroposts/src/mymodule2.js
@@ -0,0 +1,11 @@
+export const person = {
+  name: 'brad',
+  age: 36,
+}
+
+export function sayHello() {
+  return `Hello ${person.name}`
+}
+
+const greeting = 'Hello World !'
+export default greeting
